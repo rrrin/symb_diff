982a22c3e0cad17497795e831019f1bc2fd628b7
rrrin <kate.voropaeva@gmail.com>
2018-10-28 23:04:06 +0700

    изменила print, добавила mul

--- Source.cpp
+++ Source.cpp
@@ -5,15 +5,26 @@
 #include <assert.h>
 #include <vector>
 #include <exception>
-
+#include <memory>
 using namespace std;
-
+//
+//
+//
+class Expression;
+typedef shared_ptr<Expression> expr_p;
 class Expression
 {
 public:
 	virtual Expression * diff() = 0;
+//	virtual expr_p diff() = 0;
 	virtual void print() const = 0;
+	virtual bool not_zero() const
+	{
+		return true;
+	}
+	virtual ~Expression() {};
 };
+
 
 class number : public  Expression
 {
@@ -23,14 +34,17 @@ public:
 	{
 		x = a;
 	}
-	virtual ~number() {};
 	virtual number* diff() override
 	{
 		return new number(0);
 	}
 	virtual void print() const override
 	{
-		cout << "class number x = " << x << endl;
+		cout << " " << x << " " ;
+	}
+	virtual bool not_zero() const override
+	{
+		return x!=0;
 	}
 
 };
@@ -39,9 +53,9 @@ class variable : public Expression
 {
 	string name;
 public:
-	variable (string a)
+	variable()
+		: name("X")
 	{
-		name = a;
 	}
 	virtual number* diff() override
 	{
@@ -49,17 +63,20 @@ public:
 	}
 	virtual void print() const override
 	{
-		cout << "class variable name = " << name << endl;
+		cout << " " << name << " ";
 	}
 
 };
 
-class add : public Expression
+class double_oper : public Expression
 {
+protected:
 	Expression *left, *right;
+	string name_oper;
 public:
-	add(Expression *le, Expression *r)
+	double_oper(Expression *le, Expression *r, const string &oper)
 		: left(le), right(r)
+		, name_oper(oper)
 	{
 		assert(left != nullptr);
 		assert(right != nullptr);
@@ -67,10 +84,25 @@ public:
 
 	virtual void print() const override
 	{
-		cout << "class add left = " ;
-		left->print();
-		cout << " right = ";
-		right->print();
+		cout << " (";
+		if (left->not_zero())
+			left->print();
+		if (left->not_zero() && right->not_zero())
+			cout << name_oper;
+		if (right->not_zero())
+			right->print();
+		cout << ") ";
+	}
+
+};
+class add : public double_oper
+{
+public:
+	add(Expression *le, Expression *r)
+		: double_oper(le, r, " + ")
+	{
+		assert(left != nullptr);
+		assert(right != nullptr);
 	}
 
 	virtual add* diff() override
@@ -82,69 +114,89 @@ public:
 
 };
 
-class sub : Expression
+class mul : public double_oper
 {
-	Expression *left, *right;
 public:
-	sub(Expression *le, Expression *r)
-		: left(le), right(r)
+	mul(Expression *le, Expression *r)
+		: double_oper(le, r, " * ")
 	{
 		assert(left != nullptr);
 		assert(right != nullptr);
 	}
 
-	virtual void print() const override
-	{
-		cout << "class sub left = " << left << " right = " << right << endl;
-	}
-
-	virtual sub* diff() override
+	virtual add* diff() override
 	{
 		Expression *le = left->diff();
 		Expression*r = right->diff();
-		return new sub(le, r);
+		Expression *op1 = new mul(le, right);
+		Expression *op2 = new mul(left, r);
+		return new add(op1, op2);
+	}
+};
+
+class sub : public double_oper
+{
+public:
+	sub(Expression *le, Expression *r)
+		: double_oper(le, r, " - ")
+	{
+		assert(left != nullptr);
+		assert(right != nullptr);
+	}
+
+	virtual add* diff() override
+	{
+		Expression *le = left->diff();
+		Expression*r = right->diff();
+		Expression* r2 = new mul(new number(-1.), r);
+		return new add(le, r2);
 	}
 };
 
 void test(const Expression &e)
 {
+	cout << "test - ";
 	e.print();
-}
-Expression * create(int n)
-{
-	Expression * ans = nullptr;
-	switch (n)
-	{
-	case 1:
-		ans = new number(56.);
-		break;
-
-	case 2:
-		ans = new variable("rt");
-		break;
-
-	}
-	return ans;
+	cout << endl;
 }
 int main()
 {
 	number t(2.);
-	variable y("asd");
-	t.print();
-	y.print();
-
+	variable y;
 	test(t);
 	test(y);
+	{
+		// x+2
+		cout << "test  X + 2 " << endl;
+		variable x;
+		number n(2);
+		add s(&x, &n);
+		cout << "s = ";
+		s.print();
+		cout << endl;
 
-	Expression * exp = create(1);
-	exp->print();
-	delete exp;
+		Expression* s1 = s.diff();
+		cout << "s.diff = ";
+		s1->print();
+		cout << endl;
+	}
+	{
+		// 5 - X
+		expr_p x1(new variable);
+		cout << "test  5 - X " << endl;
+		variable x;
+		number n(5);
+		sub s(&n, &x);
+		cout << "s = ";
+		s.print();
+		cout << endl;
 
-// x+2
-	variable x("x");
-	number n(2);
-	add s(&x, &n);
-	s.print();
-	Expression* s1= s.diff();
-	s1->print();
+		Expression* s1 = s.diff();
+		cout << "s.diff = ";
+		s1->print();
+		cout << endl;
+	}
+
+	_getch();
+	return 0;
 }
